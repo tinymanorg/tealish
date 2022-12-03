cd995186313f55c68b0c565059a095789b74202c
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-12-03 10:00:53 -0500

    fix flake issues

--- tealish/__init__.py
+++ tealish/__init__.py
@@ -1,4 +1,4 @@
-from typing import List, Dict, Tuple, Optional, Any, Union
+from typing import List, Dict, Union
 from .base import BaseNode
 from .nodes import Node, Program
 from .utils import TealishMap
