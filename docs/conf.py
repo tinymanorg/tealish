1dca92b217eef3c80da999fe271313916d1df669
Fergal Walsh <fergal@hipolabs.com>
2023-03-06 11:38:48 +0000

    Tidy up social_links

--- docs/conf.py
+++ docs/conf.py
@@ -30,33 +30,26 @@ html_static_path = [
 ]
 
 html_css_files = [
-  'css/typography/fonts.css',
-  'css/override.css',
-  'css/custom.css',
-  'css/util-classes.css',
-  'css/code-block.css',
-  'css/custom-sidebar.css',
-  'css/right-column.css',
-  'css/searchbox.css',
-  'css/search-results.css',
-  'css/rolling-content.css',
-  'css/responsive.css',
+    "css/typography/fonts.css",
+    "css/override.css",
+    "css/custom.css",
+    "css/util-classes.css",
+    "css/code-block.css",
+    "css/custom-sidebar.css",
+    "css/right-column.css",
+    "css/searchbox.css",
+    "css/search-results.css",
+    "css/rolling-content.css",
+    "css/responsive.css",
 ]
 
 html_js_files = [
-    'js/custom.js',
-    'js/rolling-text.js',
+    "js/custom.js",
+    "js/rolling-text.js",
 ]
 
 # html_sidebars = {'**': ['localtoc.html', 'relations.html', 'sourcelink.html', 'searchbox.html']}
-html_sidebars = {'**': ['custom-sidebar.html']}
-
-social_links = [{"title": "Github", "link":
-  'https://github.com/tinymanorg/tealish'}, {"title": "Presentation", "link":
-  "https://youtu.be/R9oKjwSYuXM"}, {"title": "Twitter", "link":
-  "https://twitter.com/tinymanorg"}, {"title": "Discord", "link":
-  "https://discord.gg/wvHnAdmEv6"}, {"title": "Reddit", "link":
-  "https://www.reddit.com/r/Tinyman/"}]
+html_sidebars = {"**": ["custom-sidebar.html"]}
 
 
 # https://alabaster.readthedocs.io/en/latest/customization.html
@@ -70,4 +63,18 @@ html_theme_options = {
     "fixed_sidebar": True,
     "github_button": True,
     "github_type": "star",
+}
+
+html_context = {
+    "social_links": [
+        {"title": "Github", "link": "https://github.com/tinymanorg/tealish"},
+        {"title": "Presentation", "link": "https://youtu.be/R9oKjwSYuXM"},
+        {
+            "title": "Discord",
+            "link": "https://discord.com/channels/491256308461207573/1067861991982649404",
+        },
+        {"title": "Tinyman.org", "link": "https://tinyman.org"},
+        {"title": "Twitter", "link": "https://twitter.com/tinymanorg"},
+        {"title": "Reddit", "link": "https://www.reddit.com/r/Tinyman/"},
+    ],
 }
